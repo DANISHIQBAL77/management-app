6d41edadaa1751a9f105106d060b70cb8ffafb17
DANISHIQBAL77 <di41141455@gmail.com>
2026-01-12 17:28:26 +0500

    updated code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
